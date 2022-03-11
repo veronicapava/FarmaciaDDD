060e4cb4d27938d4cc4fc4a477dd5ef142bbf9fb
veronicapava <veronicapava@gmail.com>
2022-03-10 20:41:02 -0500

    Implementa el estado en la entidad, se desarrollan comportamientos

--- src/main/java/com/sofka/farmacia/personal/entidades/AuxiliarFarmacia.java
+++ src/main/java/com/sofka/farmacia/personal/entidades/AuxiliarFarmacia.java
@@ -3,14 +3,19 @@ package com.sofka.farmacia.personal.entidades;
 import co.com.sofka.domain.generic.Entity;
 import com.sofka.farmacia.personal.values.AuxiliarFarmaciaId;
 import com.sofka.farmacia.personal.values.DatosPersonales;
+import com.sofka.farmacia.personal.values.Estado;
 
 import java.util.Objects;
 
 public class AuxiliarFarmacia extends Entity<AuxiliarFarmaciaId> {
     private DatosPersonales datosPersonales;
+    private Estado estado;
+
+
     public AuxiliarFarmacia(AuxiliarFarmaciaId entityId, DatosPersonales datosPersonales) {
         super(entityId);
         this.datosPersonales = datosPersonales;
+        this.estado = new Estado(Estado.Fase.NO_RECIBIDO);
     }
 
     //comportamientos
@@ -20,14 +25,25 @@ public class AuxiliarFarmacia extends Entity<AuxiliarFarmaciaId> {
     }
 
     public void recibirPedido(){
+        this.estado = new Estado(Estado.Fase.RECIBIDO);
     }
 
     public void almacenarPedidoRecibido(){
+        //Regla de dominio
+        if(this.estado.value().equals(Estado.Fase.RECIBIDO)){
+            System.out.println("Se almacenó el pedido");
+        } else {
+            throw new IllegalArgumentException("No se puede almacenar el pedido, ya que no se recibió");
+        }
     }
 
-    //getter
+    //Propiedades
 
     public DatosPersonales datosPersonales() {
         return datosPersonales;
+    }
+
+    public Estado estado() {
+        return estado;
     }
 }
